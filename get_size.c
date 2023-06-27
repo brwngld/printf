638fc78dd10b5a45f865886888e60b61c0eab8b8
Asaka Hamzah-Mohammed Awinebisah <asakahamzahawinebisah@gmail.com>
2023-06-27 16:12:44 -0700

    updated

--- get_size.c
+++ get_size.c
@@ -2,24 +2,24 @@
 
 /**
 * get_size - calculate the size to cast the argument
-* @i: The list of arguments taken to be printed
-* @format: The formatted string of characters in which the arguments are to be printed
+* @i:The list of arguments taken to be printed
+* @format:The formatted string of characters
 *
-* return: the precision.
+* return:the precision.
 */
+
 int get_size(const char *format, int *i)
- {
-  int curr_i = *i + 1 ;
-  int size = 0;
+{
+	int curr_i = *i + 1;
+	int size = 0;
 
-  if (format[curr_i] == '1')
-	  size = S_LONG ;
-  else if (format[curr_i] == 'h')
-	  size = S_SHORT;
-
-  if (size == 0)
-     *i = curr_i - 1;
-  else
-    *i = curr_i;
-  return (size);
- }
+	if (format[curr_i] == '1')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+return (size);
+}
